3574048caed130e05554aa4303fd9f1a7f767b4b
Mara Kim <mara.kim@spglobal.com>
2019-01-25 19:26:32 -0500

    Add personality

---
+++ kismet/personality/core.py
@@ -0,0 +1,22 @@
+import regex
+import torch
+from torch.distributions.gamma import Gamma
+from torch.distributions.categorical import Categorical
+import math
+from typing import List
+from .responses import responses
+
+
+def analyze(string: str, responses_: List[str] = responses):
+    mentions = len(
+        regex.findall(r"[Kk]+\s*[Ii]+\s*[Ss]+\s*[Mm]+\s*[Ee]+\s*[Tt]+", string)
+    )
+    attention = math.log(mentions + 0.5) * math.log10(len(string.replace(r"\s", "")))
+    excitement = int(torch.ceil(Gamma(2.5, 1 / attention).sample()).tolist())
+
+    categorical = Categorical(torch.ones(len(responses_)))
+    response = []
+    for i in range(excitement):
+        response.append(responses_[categorical.sample()])
+
+    return " ".join(response)
