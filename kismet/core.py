3f3426e21d53ede796c1bc103185ef054c0469ee
Mara Kim <mara.kim@spglobal.com>
2019-01-25 20:58:44 -0500

    Integrate personality

--- kismet/core.py
+++ kismet/core.py
@@ -1,7 +1,18 @@
 from kismet.parser import KismetParser
+from kismet.personality.core import analyze
 
 # Create parser object.
 parser = KismetParser()
 
+
 def process(string: str):
-    return parser.parse(string)
+    parsed = parser.parse(string)
+    emoted = analyze(string)
+    if parsed and emoted:
+        return parsed + "\n" + emoted
+    elif parsed:
+        return parsed
+    elif emoted:
+        return emoted
+    else:
+        return None
