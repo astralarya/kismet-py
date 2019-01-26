c294e37298be9bb1071f03303b03f463e3a3b9ba
Mara Kim <mara.kim@spglobal.com>
2019-01-26 14:30:23 -0500

    Code block extraction

--- kismet/core.py
+++ kismet/core.py
@@ -1,3 +1,5 @@
+import regex
+
 from kismet.parser import KismetParser
 from kismet.personality.core import analyze
 
@@ -16,3 +18,46 @@ def process(string: str):
         return emoted
     else:
         return ""
+
+
+def code_blocks(string: str, syntax_type: str = "kismet"):
+    blocks = []
+    fence = None
+    ignore = False
+    for line in string.splitlines(keepends=True):
+        while line:
+            if fence:
+                # Look for fence end
+                match = regex.search(fence, line)
+                if match:
+                    if not ignore:
+                        blocks[-1] += line[: match.start()]
+                    fence = None
+                    line = line[match.end() :]
+                elif not ignore:
+                    blocks[-1] += line
+                    line = None
+                else:
+                    line = None
+            else:
+                # Look for fence start
+                match = regex.search(r"^`{3,}", line)
+                if match:
+                    fence = "^" + match.group()
+                    syntax = line[match.end() :]
+                    if syntax != "\n" and syntax != syntax_type + "\n":
+                        ignore = True
+                    else:
+                        blocks.append("")
+                    line = None
+                else:
+                    match = regex.search(r"`+", line)
+                    if match:
+                        fence = match.group()
+                        line = line[match.end() :]
+                        blocks.append("")
+                    else:
+                        line = None
+    if fence:
+        del blocks[-1]
+    return blocks
