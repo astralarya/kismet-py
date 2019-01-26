c79c82c83e352f133ac55c2a6f23519b616f3389
Mara Kim <mara.kim@spglobal.com>
2019-01-25 20:57:51 -0500

    Tune personality

--- kismet/personality/core.py
+++ kismet/personality/core.py
@@ -2,21 +2,38 @@ import regex
 import torch
 from torch.distributions.gamma import Gamma
 from torch.distributions.categorical import Categorical
+import numpy
 import math
 from typing import List
-from kismet.personality.responses import responses
+from kismet.personality.responses import responses as responses_
 
 
-def analyze(string: str, responses_: List[str] = responses):
+class Responder:
+    def __init__(self, responses=responses_):
+        self.categorical = Categorical(torch.ones(len(responses_)))
+        self.responses = responses_
+
+    def respond(self, excitement: int):
+        response = []
+        for i in range(excitement):
+            response.append(self.responses[self.categorical.sample()])
+
+        resposnse = " ".join(response)
+        return " ".join(response)
+
+
+responder_ = Responder()
+
+
+def analyze(string: str, responder: Responder = responder_):
     mentions = len(
         regex.findall(r"[Kk]+\s*[Ii]+\s*[Ss]+\s*[Mm]+\s*[Ee]+\s*[Tt]+", string)
     )
-    attention = math.log(mentions + 0.5) * math.log10(len(string.replace(r"\s", "")))
-    excitement = int(torch.ceil(Gamma(2.5, 1 / attention).sample()).tolist())
-
-    categorical = Categorical(torch.ones(len(responses_)))
-    response = []
-    for i in range(excitement):
-        response.append(responses_[categorical.sample()])
-
-    return " ".join(response)
+    if mentions:
+        attention = numpy.arcsinh(mentions) * numpy.arcsinh(
+            math.log(len(string.replace(r"\s", "")))
+        )
+        excitement = int(torch.ceil(Gamma(1.2, 2 / attention).sample()).tolist())
+        return responder.respond(excitement)
+    else:
+        return None
