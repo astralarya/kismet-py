8b9c33c7cd09578d776e86701d69a9400a05a013
Mara Kim <mara@autochthe.net>
2022-07-06 06:21:51 -0700

    Reorganize repo

---
+++ python/setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from setuptools import setup
+from kismet import __version__
+
+setup(version=__version__)
--- setup.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-
-setup(version="0.8.0")
