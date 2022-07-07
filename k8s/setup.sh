d8a77e8698705368c6c41b4a811015749a857e3a
Mara Kim <mara@autochthe.net>
2022-07-06 17:30:29 -0700

    Style and output

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -5,19 +5,15 @@ cd "$(dirname "$0")"
 
 
 printf 'Checking az login...'
-
 az account show --output none
-
 printf 'SUCCESS\n'
 
 
 printf 'Setup prod env...'
-
 az keyvault secret show \
   --name "Kismet-values" \
   --vault-name "Kismet-py" \
   --query "value" \
   --output tsv \
-  > k8s/helm/values.yaml
-
+  > helm/values.yaml
 printf 'SUCCESS\n'
--- setup.sh
+++ setup.sh
@@ -4,38 +4,27 @@ set -e
 cd "$(dirname "$0")"
 
 
-printf 'Setup python...\n'
+printf '# Setup python\n'
 
-pushd python
-./setup.sh
-popd
+./python/setup.sh
 
-printf 'SUCCESS\n'
 
+printf '# Setup k8s\n'
+
+./k8s/setup.sh
+
+
+printf '# Setup dev\n'
 
 printf 'Checking az login...'
-
 az account show --output none
-
 printf 'SUCCESS\n'
 
-
-printf 'Setup dev env...'
-
+printf 'Setup env...'
 az keyvault secret show \
   --name "Kismet-Dev-env" \
   --vault-name "Kismet-Dev" \
   --query "value" \
   --output tsv \
   > .env
-
-printf 'SUCCESS\n'
-
-
-printf 'Setup k8s...\n'
-
-pushd k8s
-./setup.sh
-popd
-
 printf 'SUCCESS\n'
